a079c4a1a51f315961cd2f519ba38f36235ab11b
Alexey_BH <alexey_bh@mail.ru>
2021-10-20 17:08:02 +0300

    Финальная версия приложения

--- HW2.1.1_Semaphore/ViewController.swift
+++ HW2.1.1_Semaphore/ViewController.swift
@@ -8,18 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Предустановленные значения прозрачности
+    private let alphaFilled: CGFloat = 1.0
+    private let alphaTransparent: CGFloat = 0.2
+    
+    // Цвета светофора
+    enum Color {
+        case colorUnderfined
+        case colorRed
+        case colorYellow
+        case colorGreen
+    }
+    
+    private var currentColor = Color.colorUnderfined
 
     @IBOutlet var lightRed: UIView!
-    
     @IBOutlet var lightYellow: UIView!
-    
     @IBOutlet var lightGreen: UIView!
-    @IBOutlet var switchButton: UIButton!
+    @IBOutlet var buttonSwitch: UIButton!
+
+    var color: Color {
+        get {
+            return currentColor
+        }
+        set(newColor) {
+            currentColor = newColor
+            switch newColor {
+            case .colorUnderfined:
+                lightRed.alpha = alphaTransparent
+                lightYellow.alpha = alphaTransparent
+                lightGreen.alpha = alphaTransparent
+            case .colorRed:
+                lightRed.alpha = alphaFilled
+                lightYellow.alpha = alphaTransparent
+                lightGreen.alpha = alphaTransparent
+            case .colorYellow:
+                lightRed.alpha = alphaTransparent
+                lightYellow.alpha = alphaFilled
+                lightGreen.alpha = alphaTransparent
+            case .colorGreen:
+                lightRed.alpha = alphaTransparent
+                lightYellow.alpha = alphaTransparent
+                lightGreen.alpha = alphaFilled
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.color = .colorUnderfined
     }
+        
+    @IBAction func buttonSwitchPressed(_ sender: Any) {
+        self.switchColor()
+    }
+    
+    func switchColor() {
+        switch self.color {
+        case .colorUnderfined:
+            self.color = .colorRed
+        case .colorRed:
+            self.color = .colorYellow
+        case .colorYellow:
+            self.color = .colorGreen
+        case .colorGreen:
+            self.color = .colorRed
+        }
+        
 
+        
+    }
+    
 
 }
 
